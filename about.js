82d234f7a06a58976c7b1caf3a3f7d2c220a85f2
Kumar Anirudha <anirudhastark@yahoo.com>
2018-07-01 17:24:04 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,5 +1,5 @@
 var converter = new showdown.Converter();
-$.get( "https://raw.githubusercontent.com/acycliclabs/seedgen/master/README.md", function( inputText ) {
+$.get( "https://raw.githubusercontent.com/impactdlt/seedgen/master/README.md", function( inputText ) {
     var convertedHtml = converter.makeHtml(inputText);
     console.log('convertedHtml', convertedHtml);
     $('#about-div').html(convertedHtml);
